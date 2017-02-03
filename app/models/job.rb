1ebf6216f889ed627f897569ce227ea1036e0836
klpk011 <sophieQ011@gmail.com>
2017-02-04 07:23:05 +0800

    upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,7 +4,7 @@ class Job < ApplicationRecord
  validates :wage_lower_bound, presence: true
  validates :wage_lower_bound, numericality: { greater_than: 0 }
 #validates :wage_upper_bound, numericality: { greater_than: :wage_lower_bound }
-
+has_many :resumes
 def publish!
   self.is_hidden = false
   self.save
